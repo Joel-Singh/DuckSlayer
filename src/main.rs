5c6d957ab2ab8f43c38f67bdac0626caf3c4a24e
Joel Singh <joelsingh788@gmail.com>
2025-04-15 18:24:39 -0400

    Format

--- src/main.rs
+++ src/main.rs
@@ -15,21 +15,27 @@ fn setup_camera(mut commands: Commands) {
 }
 
 fn spawn_quacka(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn(( Sprite {
-        image: asset_server.load("quacka.png"),
-        custom_size: Some(Vec2::new(100.0, 100.0)),
-        ..default()
-    }, Transform {
+    commands.spawn((
+        Sprite {
+            image: asset_server.load("quacka.png"),
+            custom_size: Some(Vec2::new(100.0, 100.0)),
+            ..default()
+        },
+        Transform {
             translation: Vec3::new(0., 200., 0.),
             ..default()
-        } ));
+        },
+    ));
 
-    commands.spawn(( Sprite {
-        image: asset_server.load("princess_tower.png"),
-        custom_size: Some(Vec2::new(50., 50.)),
-        ..default()
-    }, Transform {
+    commands.spawn((
+        Sprite {
+            image: asset_server.load("princess_tower.png"),
+            custom_size: Some(Vec2::new(50., 50.)),
+            ..default()
+        },
+        Transform {
             translation: Vec3::new(0., 0., 0.),
             ..default()
-        } ));
+        },
+    ));
 }
