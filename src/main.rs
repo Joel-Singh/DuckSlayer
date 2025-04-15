d09f71162dfb793224eec8bd091f0b2da9c68ebb
Joel Singh <joelsingh788@gmail.com>
2025-04-15 18:21:23 -0400

    Spawn quakka programmer art sprite

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins.set(bevy::log::LogPlugin {
+            //level: bevy::log::Level::DEBUG,
+            ..default()
+        }))
+        .add_systems(Startup, (setup_camera, spawn_quacka))
+        .run();
+}
+
+fn setup_camera(mut commands: Commands) {
+    commands.spawn(Camera2d);
+}
+
+fn spawn_quacka(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(( Sprite {
+        image: asset_server.load("quacka.png"),
+        custom_size: Some(Vec2::new(100.0, 100.0)),
+        ..default()
+    }, Transform {
+            translation: Vec3::new(0., 200., 0.),
+            ..default()
+        } ));
 }
