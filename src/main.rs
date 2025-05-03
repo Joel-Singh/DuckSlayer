8ab482f67eff1b676ed596a4c7230a265ada774c
Joel Singh <joelsingh788@gmail.com>
2025-05-02 20:29:38 -0400

    Fix typo

--- src/main.rs
+++ src/main.rs
@@ -72,7 +72,7 @@ fn setup_camera(mut commands: Commands) {
 fn tick_attacker_cooldowns(mut attackers: Query<&mut Attacker>, time: Res<Time>) {
     for mut attacker in attackers.iter_mut() {
         if attacker.cooldown.mode() == TimerMode::Repeating {
-            panic!("Attack coolodwn should be once");
+            panic!("Attack cooldown should be once");
         }
         attacker.cooldown.tick(time.delta());
     }
