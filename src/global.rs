126c74a6d530f8a60a1902c04331cae955ae4a13
Joel Singh <joelsingh788@gmail.com>
2025-05-16 21:31:00 -0400

    Implement CursorWorldCoords resource

--- src/global.rs
+++ src/global.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, window::PrimaryWindow};
 
 #[derive(Clone, Copy, PartialEq, Eq, Hash, Debug, Default, States)]
 pub enum GameState {
@@ -7,11 +7,32 @@ pub enum GameState {
     InGame,
 }
 
+#[derive(Resource, Default)]
+struct CursorWorldCoords(Vec2);
+
 pub const SCREEN_WIDTH: f32 = 1366.0;
 pub const SCREEN_HEIGHT: f32 = 768.0;
 
 pub const DECK_WIDTH: f32 = 0.1 * SCREEN_WIDTH;
 
 pub fn global(app: &mut App) {
-    app.init_state::<GameState>();
+    app.add_systems(FixedUpdate, update_cursor_world_coords)
+        .init_resource::<CursorWorldCoords>()
+        .init_state::<GameState>();
+}
+
+fn update_cursor_world_coords(
+    windows_q: Single<&Window, With<PrimaryWindow>>,
+    camera_q: Single<(&GlobalTransform, &Camera)>,
+    mut cursor_world_coords_res: ResMut<CursorWorldCoords>,
+) {
+    let cursor_window_position = windows_q.cursor_position();
+    if let Some(cursor_window_position) = cursor_window_position {
+        let (camera_transform, camera) = *camera_q;
+        if let Ok(cursor_world_coords) =
+            camera.viewport_to_world_2d(camera_transform, cursor_window_position)
+        {
+            cursor_world_coords_res.0 = dbg!(cursor_world_coords);
+        }
+    }
 }
