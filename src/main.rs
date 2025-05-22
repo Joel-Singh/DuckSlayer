20decda6481405835d95ee456be73fba29f86833
Joel Singh <joelsingh788@gmail.com>
2025-05-22 15:42:21 -0400

    Override resolution to 1.0 if joels computer

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,8 @@ use manage_level::*;
 use titlescreen::*;
 use troops::*;
 
+use std::env;
+
 fn main() {
     App::new()
         .add_plugins(
@@ -23,7 +25,7 @@ fn main() {
                 })
                 .set(WindowPlugin {
                     primary_window: Some(Window {
-                        resolution: WindowResolution::default().with_scale_factor_override(1.0),
+                        resolution: get_resolution(),
                         ..default()
                     }),
                     ..default()
@@ -40,4 +42,20 @@ fn main() {
 
 fn setup_camera(mut commands: Commands) {
     commands.spawn(Camera2d);
+}
+
+// Resolution is incorrectly found on joel's computer
+fn get_resolution() -> WindowResolution {
+    let mut is_joels_computer = false;
+    if let Ok(joels_comuter_env) = env::var("JOELS_COMPUTER") {
+        if joels_comuter_env == "true" {
+            is_joels_computer = true;
+        }
+    }
+
+    let mut resolution = WindowResolution::default();
+    if is_joels_computer {
+        resolution = resolution.with_scale_factor_override(1.0);
+    }
+    resolution
 }
