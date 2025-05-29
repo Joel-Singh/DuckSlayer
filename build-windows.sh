05cfea4f7a17c4aebafac0a3b8b19856653b7f0b
Joel Singh <joelsingh788@gmail.com>
2025-05-28 22:31:48 -0400

    Create build windows script

---
+++ build-windows.sh
@@ -0,0 +1,10 @@
+set -e
+
+cd $(git rev-parse --show-toplevel)
+cargo build --target=x86_64-pc-windows-gnu --release
+trash-put output
+mkdir output
+cp target/x86_64-pc-windows-gnu/release/DuckSlayer.exe ./output
+cp -r assets/*.png ./output
+zip output/DuckSlayer.zip output/*
+echo DONE, see DuckSlayer.zip
