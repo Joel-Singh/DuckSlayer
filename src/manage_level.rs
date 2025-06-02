56e496b8a7c4916a8778ac69ceeaea00e4db64f1
Joel Singh <joelsingh788@gmail.com>
2025-06-02 13:22:52 -0400

    Move debug_ui to bottom of file

--- src/manage_level.rs
+++ src/manage_level.rs
@@ -206,6 +206,45 @@ fn set_gameover_false(mut gameover: ResMut<NextState<GameOver>>) {
     gameover.set(GameOver::False);
 }
 
+mod game_messages {
+    use bevy::{
+        ecs::{schedule::ScheduleConfigs, system::ScheduleSystem},
+        prelude::*,
+    };
+
+    #[derive(Component)]
+    struct MessageBox;
+
+    pub fn game_messages(app: &mut App) {
+        app.add_systems(Startup, spawn_message_box);
+    }
+
+    fn spawn_message_box(mut commands: Commands) {
+        commands.spawn((
+            Text::new(""),
+            Node {
+                margin: UiRect::all(Val::Auto).with_top(Val::Px(80.)),
+                ..default()
+            },
+            BoxShadow::new(
+                Color::BLACK.with_alpha(0.8),
+                Val::ZERO,
+                Val::ZERO,
+                Val::Percent(0.),
+                Val::Px(10.),
+            ),
+            MessageBox,
+        ));
+    }
+
+    pub fn set_message(message: &'static str) -> ScheduleConfigs<ScheduleSystem> {
+        return (move |mut commands: Commands, message_box: Single<Entity, With<MessageBox>>| {
+            commands.entity(*message_box).insert(Text::new(message));
+        })
+        .into_configs();
+    }
+}
+
 mod debug_ui {
     use bevy::{ecs::system::RunSystemOnce, prelude::*};
     use bevy_egui::{
@@ -261,44 +300,5 @@ mod debug_ui {
                 commands.queue(PushToDeckbar(card));
             }
         }
-    }
-}
-
-mod game_messages {
-    use bevy::{
-        ecs::{schedule::ScheduleConfigs, system::ScheduleSystem},
-        prelude::*,
-    };
-
-    #[derive(Component)]
-    struct MessageBox;
-
-    pub fn game_messages(app: &mut App) {
-        app.add_systems(Startup, spawn_message_box);
-    }
-
-    fn spawn_message_box(mut commands: Commands) {
-        commands.spawn((
-            Text::new(""),
-            Node {
-                margin: UiRect::all(Val::Auto).with_top(Val::Px(80.)),
-                ..default()
-            },
-            BoxShadow::new(
-                Color::BLACK.with_alpha(0.8),
-                Val::ZERO,
-                Val::ZERO,
-                Val::Percent(0.),
-                Val::Px(10.),
-            ),
-            MessageBox,
-        ));
-    }
-
-    pub fn set_message(message: &'static str) -> ScheduleConfigs<ScheduleSystem> {
-        return (move |mut commands: Commands, message_box: Single<Entity, With<MessageBox>>| {
-            commands.entity(*message_box).insert(Text::new(message));
-        })
-        .into_configs();
     }
 }
