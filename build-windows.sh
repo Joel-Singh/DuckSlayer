7200f8e557661744330935962c269edce8bf4238
Joel Singh <joelsingh788@gmail.com>
2025-06-24 14:19:41 -0400

    Copy all of assets into output

--- build-windows.sh
+++ build-windows.sh
@@ -5,6 +5,6 @@ cargo build --target=x86_64-pc-windows-gnu --release
 trash-put output
 mkdir output
 cp target/x86_64-pc-windows-gnu/release/DuckSlayer.exe ./output
-cp -r assets/*.png ./output
+cp -r assets ./output
 zip output/DuckSlayer.zip output/*
 echo DONE, see DuckSlayer.zip
