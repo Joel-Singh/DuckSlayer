99835d9ce418a2d0a8eaac7489b52c250602e762
Joel Singh <joelsingh788@gmail.com>
2025-07-11 12:18:05 -0400

    Remove containing output directory from zip

--- build-windows.sh
+++ build-windows.sh
@@ -6,5 +6,6 @@ trash-put output
 mkdir output
 cp target/x86_64-pc-windows-gnu/release/DuckSlayer.exe ./output
 cp -r assets ./output
-zip output/DuckSlayer.zip output/*
+cd output
+zip DuckSlayer.zip DuckSlayer.exe assets
 echo DONE, see DuckSlayer.zip
